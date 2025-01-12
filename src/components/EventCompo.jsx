aaf66a9ad997876c20a5f28e7fbf79ba072eb602
Pranav Molawade <pranavmolawade123@gmail.com>
2025-01-12 16:47:58 +0530

    verce

--- src/components/EventCompo.jsx
+++ src/components/EventCompo.jsx
@@ -140,7 +140,7 @@ const EventCompo = () => {
                     {/* Event Details */}
                     {event.banner && (
                       <img
-                        src={`http://localhost:8000/${event.banner}`}
+                        src={`backendeventurl.vercel.app/${event.banner}`}
                         alt="Event Banner"
                         className="w-full h-48 object-cover rounded-md mb-4"
                       />
